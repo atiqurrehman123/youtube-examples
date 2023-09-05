59316d612af788c2780655584e156275e717dffc
atiq ur Rehman <atiqu555@gmail.com>
2023-09-05 11:20:28 +0500

    redux-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './tuotorials/reduxExample/Counter';
 
 function App() {
   return (
     <div className="App">
-     
+     {/* Redux Render Component */}
+     <Counter/>
     </div>
   );
 }
